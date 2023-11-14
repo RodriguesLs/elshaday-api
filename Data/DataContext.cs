1315b3cb98b778bceac0d58dc92e281e0d015b28
Rodrigues <fr.costa95@gmail.com>
2023-11-14 17:59:34 -0300

    feat(db, configs): configure database into project
        Created Data folder to organize dbContext.
        Included into appsettings connection_string.

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace elshaday_test_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using elshaday_test_api.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace elshaday_test_api
 {
     public class Program
@@ -7,6 +10,9 @@ namespace elshaday_test_api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
