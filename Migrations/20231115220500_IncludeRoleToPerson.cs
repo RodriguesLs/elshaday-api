631f6c3be18719336c1a8349430561a210437fcf
Rodrigues <fr.costa95@gmail.com>
2023-11-15 21:33:09 -0300

    feat(models, migrations): include role to Person
        Included new attribute (role) to Person.
        Generated Migrations to modify database.

---
+++ Migrations/20231115220500_IncludeRoleToPerson.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace elshaday_test_api.Migrations
+{
+    public partial class IncludeRoleToPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "People",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "People");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -8,11 +8,20 @@
             juridic
         }
 
+        public enum RoleType
+        {
+            customer,
+            supplier,
+            employee
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public PersonType Type { get; set; }
         public string Document {  get; set; }
-        public string Nickname { get; set; }
+        public string? Nickname { get; set; }
+
+        public RoleType Role { get; set; }
         public List<Address> Addresses { get; set; } = new List<Address>();
     }
 }
